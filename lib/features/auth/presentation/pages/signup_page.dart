685d683bcf0b2339fcd5a9735d42db16e20f8f06
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-23 20:59:28 +0530

    Add SignupPage widget for user registration with form fields and validation

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_auth_with_bloc/core/theme/app_palette.dart';
+import 'package:learn_flutter_auth_with_bloc/features/auth/presentation/widgets/auth_field.dart';
+import 'package:learn_flutter_auth_with_bloc/features/auth/presentation/widgets/auth_button.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final firstNameController = TextEditingController(); //to access 
+  final lastNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() { //all controller must dispose
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30),
+          
+              AuthField(hintText: 'First Name', controller: firstNameController,),
+              SizedBox(height: 15),
+          
+              AuthField(hintText: 'Last Name', controller: lastNameController,),
+              SizedBox(height: 15),
+          
+              AuthField(hintText: 'Email', controller: emailController,),
+              SizedBox(height: 15),
+          
+              AuthField(hintText: 'Password', controller: passwordController, isObscureText: true,),
+              SizedBox(height: 20),
+          
+              AuthButton(buttonText: "Sign Up",),
+              SizedBox(height: 15),
+          
+              RichText(
+                text: TextSpan(
+                  text: 'Already have and account?',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: ' Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPalette.blueColor,
+                        fontWeight: FontWeight.bold
+                      )
+                    )
+                  ]
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
