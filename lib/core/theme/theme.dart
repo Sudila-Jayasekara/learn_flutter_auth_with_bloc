e416730be87ecbb1011fe80098789bf7ae52b418
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-23 20:58:45 +0530

    Add AppPalette and AppTheme classes for consistent theming

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_auth_with_bloc/core/theme/app_palette.dart';
+
+class AppTheme {
+  static _border([Color color = AppPalette.borderColor]) => OutlineInputBorder(
+    borderSide: BorderSide(color: color, width: 1),
+    borderRadius: BorderRadius.circular(10),
+  );
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    // scaffoldBackgroundColor: AppPalette.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(20),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPalette.blueColor),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(double.maxFinite, 55),
+        backgroundColor: AppPalette.blueColor,
+        foregroundColor: AppPalette.whiteColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
+}
