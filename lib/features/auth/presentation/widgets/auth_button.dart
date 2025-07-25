95d553ab39a954396aeb6e6f408f216319a1db4f
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-25 20:18:02 +0530

    feat: Update AuthButton and AuthField to support loading state and custom validation

--- lib/features/auth/presentation/widgets/auth_button.dart
+++ lib/features/auth/presentation/widgets/auth_button.dart
@@ -2,13 +2,30 @@ import 'package:flutter/material.dart';
 
 class AuthButton extends StatelessWidget {
   final String buttonText;
-  const AuthButton({super.key, required this.buttonText});
+  final VoidCallback? onPressed;
+  final bool isLoading;
+
+  const AuthButton({
+    super.key,
+    required this.buttonText,
+    this.onPressed,
+    this.isLoading = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
-      child: Text(buttonText),
+      onPressed: isLoading ? null : onPressed,
+      child: isLoading
+          ? const SizedBox(
+              height: 20,
+              width: 20,
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            )
+          : Text(buttonText),
     );
   }
 }
