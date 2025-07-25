54db44d8314bd189c9b899db7707169b1ab04f81
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-25 20:15:56 +0530

    feat: Initialize dependencies and set up authentication state management with Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_flutter_auth_with_bloc/core/di/%20service_locator.dart';
 import 'package:learn_flutter_auth_with_bloc/core/router/app_router.dart';
 import 'package:learn_flutter_auth_with_bloc/core/theme/theme.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:learn_flutter_auth_with_bloc/features/auth/presentation/bloc/auth_bloc.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => sl<AuthBloc>()..add(AuthStatusChecked()),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,11 +25,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Auth App',
-      theme: AppTheme.darkThemeMode,
-      routerConfig: AppRouter.router,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        // Handle global navigation based on auth state
+        if (state is AuthAuthenticated) {
+          AppRouter.router.go('/home');
+        } else if (state is AuthUnauthenticated) {
+          AppRouter.router.go('/signin');
+        }
+      },
+
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Auth App',
+        theme: AppTheme.darkThemeMode,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
